d9a88f57ae9e1d27e84fd99b54f7958b28db61a7
Choi Byeong Kwon <cbg293@naver.com>
2016-04-06 13:26:04 +0900

    W6_sum3+5
    W6_sum3+5

---
+++ W6_SUM3+5.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[59]:
+
+res1=0
+for i in range(1,1000):
+    if i%3==0:
+        res1=res1 + i
+print res1
+        
+
+
+# In[54]:
+
+res2=0
+for i in range(1,1000):
+    if i%5==0:
+        res2=res2 + i
+print res2
+
+
+# In[55]:
+
+res1+res2
+
